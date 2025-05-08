8b837e55546bc7ef4ba797a6cc4bb6b0e145fdaa
DOCSDROPT <Godentel@proton.me>
2025-05-08 02:16:12 +0000

    Update ESL.ps1

--- OS/ESL.ps1
+++ OS/ESL.ps1
@@ -1,6 +1,3 @@
-# Nécessite d'être lancé en tant qu'Administrateur
-
-# Import de la signature NtSetSystemInformation
 $signature = @"
 using System;
 using System.Runtime.InteropServices;
@@ -16,12 +13,22 @@ public class NativeMethods {
 
 Add-Type $signature
 
-# Constantes
 $SystemMemoryListInformation = 0x50
 $MemoryPurgeStandbyList       = 4
 
-# Lecture de l'intervalle
-$interval = Read-Host "Intervalle entre chaque purge (en secondes)"
+Write-Host " "
+Write-Host "   60   / 1  Min   |   660  / 11 Min   |   1260  / 21 Min"
+Write-Host "   120  / 2  Min   |   720  / 12 Min   |   1320  / 22 Min"
+Write-Host "   180  / 3  Min   |   780  / 13 Min   |   1380  / 23 Min"
+Write-Host "   240  / 4  Min   |   840  / 14 Min   |   1440  / 24 Min"
+Write-Host "   300  / 5  Min   |   900  / 15 Min   |   1500  / 25 Min"
+Write-Host "   360  / 6  Min   |   960  / 16 Min   |   1560  / 26 Min"
+Write-Host "   420  / 7  Min   |   1020 / 17 Min   |   1620  / 27 Min"
+Write-Host "   480  / 8  Min   |   1080 / 18 Min   |   1680  / 28 Min"
+Write-Host "   540  / 9  Min   |   1140 / 19 Min   |   1740  / 29 Min"
+Write-Host "   600  / 10 Min   |   1200 / 20 Min   |   1800  / 30 Min"
+Write-Host " "
+$interval = Read-Host "Intervalle entre chaque purge (en secondes) "
 if (-not ([double]::TryParse($interval, [ref]$null)) -or [double]$interval -le 0) {
     Write-Error "Intervalle invalide"
     exit
